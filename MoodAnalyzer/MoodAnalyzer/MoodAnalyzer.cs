c8d1758e71264ffd10f53fd2d3060895867e9cde
akshata2005 <akshatasawant1805@gmail.com>
2022-02-08 17:23:45 +0530

    [Akshata] Add: Analyze and response happy or sad mood

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string message; //instance variable
+        public MoodAnalyzer (string message)
+        {
+            this.message = message;
+        }
+    }
+    public string Analyzer()//
+    {
+        if(this.message.ToLower().Contains("happy"))
+        {
+            return "happy";
+        }
+    }
+}
