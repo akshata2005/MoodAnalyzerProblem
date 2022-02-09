81ad235ef4ef722b6b860537cc4a781536a76b50
akshata2005 <akshatasawant1805@gmail.com>
2022-02-09 10:16:27 +0530

    [Akshata] Add: Analyze and response happy or sad mood

---
+++ MoodAnalyzer/MoodAnalyzerTestcase/MoodAnalyzerTestcase/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyzerTestcase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodAnalyzerTestcase")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d45b73cf-1bbf-4ee0-be5c-693633c7ae04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
